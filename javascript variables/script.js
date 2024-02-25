e2e20c9a8f5a5c6e91a09c2d24aea9d0bac2bafa
jaymangwani777 <jaymangwani5@gmail.com>
2024-02-25 16:45:00 +0530

    add to course

---
+++ javascript variables/script.js
@@ -0,0 +1,27 @@
+console.log("hey this is tutorial 2 of javascript")
+
+var a=5;
+var b=6;
+// var c=8;
+// var d=23;
+// var e=4;
+var c="Jay"
+// variable is basically a container which stores datatypes and value inside double coat is a string
+// double coma means double coat and inside double coat value is a string 
+
+console.log(a+b)
+
+console.log(typeof a ,typeof b ,typeof c)
+// this typeof code we write because we want to know datatype of value which is stored under a variable
+// console.log(d+b)
+
+// console.log(d+b+e)
+
+// var _a="shubam";   
+// that is correct
+
+// var 55a="jay"
+// that is incorrect 
+// and why they are correct and incorrect just because it is a rule var cannot be begin with variables
+
+// jay and Jay both are different variables because starting of names is differennt first one is starting with small letter and the second one is starting with a capital letter variables are case sensitive!
