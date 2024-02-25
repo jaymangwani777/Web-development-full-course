b0fd3a9a23a7f8beb544fff3362671f2817ece07
jaymangwani777 <jaymangwani5@gmail.com>
2024-02-25 15:29:05 +0530

    add to course

---
+++ introduction to javascript/new.js
@@ -0,0 +1,4 @@
+console.log("code is running..")
+console.log("hello world")
+console.log("code is also running..")
+console.log("code is running like a waow..")
---
+++ introduction to javascript/script.js
@@ -0,0 +1,23 @@
+alert("hello world")
+// that gives us a alert box
+
+console.log("code is running..")
+console.log("code is also running..")
+console.log("code is running like a waow..")
+
+var a = prompt("Enter your number")
+var istrue = confirm(" Are you sure you want to leave this page and blast your computer ")
+
+if(istrue){
+    console.log("Computer is blasting ")
+}
+
+else{
+    console.log("computer is not blasting")
+}
+
+document.title = "Hey i am good"
+// you can change title by using Js like this
+
+document.body.style.backgroundColor="red"
+// you can change color or add styling by using javascript like this 
