5afc42090ede0651715f5df329a471e2a61dc9a0
jaymangwani777 <jaymangwani5@gmail.com>
2024-04-26 19:31:01 +0530

    add to course

---
+++ Js if else/script.js
@@ -0,0 +1,44 @@
+console.log("hello i am conditional tutorial!")
+
+
+let age=9;
+let grace= 2;
+
+// These are javascript assignment operators!
+// age=grace
+// age+=grace
+// age-=grace
+// age/=grace
+ 
+// these are operators in javascript 
+// console.log(age)
+// addition
+// console.log(age-grace)
+// subtration
+// console.log(age/grace)
+// devision
+// console.log(age*grace)
+// multiplication
+// console.log(age**grace)
+// exponentitation what it will do is the first number will remain a normal number but the second number will convert into the power of first number
+// console.log(age%grace)
+// modulus what it will do it it will devide both the numbers and the remainder will be the modulus.
+
+if((age+grace)>18) {
+    console.log("you can drive");
+}
+
+else{
+    console.log("you cannot drive");
+}
+
+// let aage=44;
+
+// if(aage<45){
+//     console.log("you cannot drive")
+// }
+
+// else{
+//     console.log("you can drive")
+// }
+
