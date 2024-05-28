c98866c74018b7200adfd93987c8b93e80f11643
jaymangwani777 <jaymangwani5@gmail.com>
2024-05-28 13:35:29 +0530

    add to course

---
+++ javascript loops/script.js
@@ -0,0 +1,14 @@
+    console.log("i am a toutorial on loops")
+
+
+    let a=5
+
+    console.log(a+1);
+    console.log(a+4);
+    console.log(a+5);
+
+
+      for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+      }
