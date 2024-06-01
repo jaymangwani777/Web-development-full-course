4bf70be1edeb0e96fbad965c2dc91c819a64e82f
jaymangwani777 <jaymangwani5@gmail.com>
2024-06-01 20:35:53 +0530

    add to course

--- javascript loops/script.js
+++ javascript loops/script.js
@@ -19,6 +19,54 @@ let a=2;
 // }
 
 
-for (let i = 0; i < 100 ; i++) {
-  console.log(i)
-}
+// for (let i = 0; i < 100 ; i++) {
+//   console.log(i)
+// }
+
+    // for (let i = 0; i <1000  ; i++) {
+    //   console.log(i + a);
+      
+    // }
+
+//     let obj= {
+//       name: "jay",
+//       role:"programmer",
+//       company : "codewithharryAI"
+//     }
+//   // this is for in loop
+//  for (const key in obj) {   
+//     console.log(key,)
+//   }
+ 
+//   // this is for of loop
+//   for (const character of "jay") {
+//     console.log(character)
+    
+//   }
+
+// let i =5;
+// while (i<6) {
+//   console.log(i)
+//   i++;
+// }
+
+// in this code one condition is true and one condition is false that's why this code is only printing i as 5 the first true condition is (i<6) the second false condition is i++ because it will add 1 in i then (i>6) will be false because now i becomes 6 and 6 is not less than 6.
+
+// let i=0;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+
+// now both the conditions are true that's why it is printing from zero to all the way to 5
+let i=0;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
+
+// this is do while loop
+
+// in this loop what code will do is it will print the the first condition irrespective of checking the other condition 
+
+// it will check the looping condition after printing its first task if the condition is true then it will print numbers from 0 to 5 and if it is not true it will only print first let i=0; that means it will print only zero
