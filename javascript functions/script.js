b9bfa9a276f3ed5c97ede4c4114001659fdd0a4a
jaymangwani777 <jaymangwani5@gmail.com>
2024-06-01 21:29:52 +0530

    add to course

---
+++ javascript functions/script.js
@@ -0,0 +1,33 @@
+// console.log("hey jay you are nice")
+// console.log("hey jay you are good looking")
+// console.log("hey jay you have nice body")
+// console.log("hey jay you are built different")
+
+function changename(name) {
+    console.log("hey " + name + " you are nice")
+    console.log("hey " + name + " you are good looking")
+    console.log("hey " + name + " you have nice body")
+    console.log("hey " + name + " you are built different")
+}
+
+// this is the function we made
+
+// in the place of name we have concadinated the string that's how we concadinate the string ! string is which is inside the double cote it is mandatory to concadinate the string for making a function or writing a function as we can say!
+
+changename("disha")
+
+
+// why we are using funcition?
+
+// because we don't want to be repatative with our code!
+
+// suppose we wrote 1000 lines of code and we have to change one or two things from those codes but apart from these one or two things the rest of code will be the same how we can do it ?
+
+// the answer is we have to make a function after we made the function we can make changes in that function then the 1000 lines of code will change automatically!
+
+function sum(a,b) 
+{ console.log(a+b);
+    
+}
+
+sum(3, 5)
